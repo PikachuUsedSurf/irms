f3e75f740c528ae59118284e52cc99e4bd784efa
PikachuUsedSurf <nanaosafokissi@gmail.com>
2023-05-25 13:58:29 +0300

    Added steps for create report creation navigation

--- app/components/modals/ReportModal.tsx
+++ app/components/modals/ReportModal.tsx
@@ -1,6 +1,7 @@
 "use client";
 import useReportModal from '@/app/hooks/useReportModal'
 import Modal from './Modal';
+import { useMemo, useState } from 'react';
 
 enum STEPS {
   category = 0,
@@ -10,19 +11,45 @@ enum STEPS {
   title = 4,
   body = 5,
   published = 6,
-
 }
 
 const ReportModal = () => {
   const reportModal = useReportModal();
+
+  const [step, setStep] = useState(STEPS.category);
+
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setStep((value) => value + 1);
+  }
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.published) {
+      return 'create';
+    }
+    return 'next';
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.category) {
+      return undefined;
+    }
+
+    return 'Back';
+  }, [step]);
   
   return (
       <Modal
-          isOpen={reportModal.isOpen}
-          onClose={reportModal.onClose}
-          onSubmit={reportModal.onClose}
-          actionLabel="submit"
-        title="Create Reports"/>
+      isOpen={reportModal.isOpen}
+      onClose={reportModal.onClose}
+      onSubmit={reportModal.onClose}
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.category ? undefined : onBack}
+      title="Create Reports"/>
   )
 }
 
