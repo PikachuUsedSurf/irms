1497edd8bb24bc644f91e4da2ae457a228bfd2c8
PikachuUsedSurf <nanaosafokissi@gmail.com>
2023-05-25 13:26:30 +0300

    Report Modal

--- app/components/modals/ReportModal.tsx
+++ app/components/modals/ReportModal.tsx
@@ -2,16 +2,27 @@
 import useReportModal from '@/app/hooks/useReportModal'
 import Modal from './Modal';
 
-const ReportModal = () => {
+enum STEPS {
+  category = 0,
+  location = 1,
+  people = 2,
+  file = 3,
+  title = 4,
+  body = 5,
+  published = 6,
 
-    const reportModal = useReportModal();
+}
+
+const ReportModal = () => {
+  const reportModal = useReportModal();
+  
   return (
       <Modal
           isOpen={reportModal.isOpen}
           onClose={reportModal.onClose}
           onSubmit={reportModal.onClose}
           actionLabel="submit"
-        title="Reports"/>
+        title="Create Reports"/>
   )
 }
 
